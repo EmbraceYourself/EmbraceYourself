52e8dc4ef0607d172d60ffcb83951d034918f66a
shindigira <shindigira@gmail.com>
2016-03-31 20:08:58 -0700

    separation of concerns -- usermodel.js server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var tableSure = require('./server/models/users/userModel.js');
 
 var app = express();
 var port = process.env.PORT || 8000;
@@ -7,9 +8,9 @@ var port = process.env.PORT || 8000;
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
-})); 
+}));
 app.use(express.static(__dirname + '/client'));
-app.use('/scripts', express.static(__dirname + '/bower_components'));
+//app.use('/scripts', express.static(__dirname + '/bower_components'));
 
 app.get('/', function(req, res){
   res.send("Rideshare server up and running!");
@@ -19,4 +20,5 @@ app.get('/', function(req, res){
 
 app.listen(port, function() {
   console.log('App up and running on http://localhost: ', port);
+  tableSure();
 });
--- server/models/users/userModel.js
+++ server/models/users/userModel.js
@@ -1,19 +1,26 @@
-var User = sequelize.define('user', {
-  firstName: {
-    type: Sequelize.STRING,
-    field: 'first_name' // Will result in an attribute that is firstName when user facing but first_name in the database
-  },
-  lastName: {
-    type: Sequelize.STRING
-  }
-}, {
-  freezeTableName: true // Model tableName will be the same as the model name
-});
+// PostgresQL database Setup
+// Official Documentation: https://github.com/brianc/node-postgres/wiki/pg
+// Guide: http://mherman.org/blog/2015/02/12/postgresql-and-nodejs/
 
-User.sync({force: true}).then(function () {
-  // Table created
-  return User.create({
-    firstName: 'John',
-    lastName: 'Hancock'
+var pg = require('pg');
+// var conString = "postgres://username:password@localhost/database";
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/test';
+
+module.exports = function  () {
+
+  var client = new pg.Client(connectionString);
+
+  client.connect(function(err) {
+    if(err) {
+      return console.error('could not connect to postgres', err);
+    }
+    client.query('CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY KEY, firstname VARCHAR(40), lastname VARCHAR(40))');
+    client.query('CREATE TABLE IF NOT EXISTS messages(id SERIAL PRIMARY KEY, text VARCHAR(40))');
+    client.end();
   });
-});
+
+}
+
+// module.exports = {
+//   tableSure: tableSure
+// };
